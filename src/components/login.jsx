1cdc58a96b9a9f8374dab1ddec79ee6afd46fa2c
Parth Awasthi <parthawasthi7478@gmail.com>
2024-10-21 16:03:57 +0530

    fixed login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { Link } from 'react-router-dom';
 function Login() {
   const [loginData, setLoginData] = useState({
     email: '',
@@ -19,13 +19,17 @@ function Login() {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-            quasi. In deleniti eaque aut repudiandae et a id nisi.
-          </p>
-        </div>
+      <div className="flex flex-col justify-left text-center lg:text-left">
+  <h1 className="text-5xl font-bold">Login now!</h1>
+  <p className="py-6">
+    Unlock Your Future with Workfolio! Log in now for personalized job listings, career tools, and networking that brings your dream job closer than ever.
+  </p>
+  <div className='flex justify-center lg:justify-start items-center gap-1'>
+    <p className='py-0'>Don't have an account?</p>
+    <Link to="/register" className="link link-primary">Register here!</Link>
+  </div>
+</div>
+
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <form onSubmit={handleSubmit} className="card-body">
             <div className="form-control">
